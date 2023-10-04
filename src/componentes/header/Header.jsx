e0606af0f5e28d86c0e007de16149ade1ecab1e4
Marciohenr1que <xxmarcio.henrique@gmail.com>
2023-10-04 19:37:15 -0300

    atualização

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import './styleHeader.css'
+import  { useState } from 'react'
 import { BsYoutube } from 'react-icons/bs'
 import { AiFillLinkedin } from 'react-icons/ai'
-import{AiOutlineInstagram} from 'react-icons/ai'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { BsList } from 'react-icons/bs'
 function Header() {
+  const [showNav, setShowNav] = useState(false)
+
+    const toggleNav = () => {
+        setShowNav(!showNav)
+    };
   return (
-    <div class="teste">
-      <img src="/logotipo.png" alt="logotipo" class="w-12 h-12" />
-      <div class="menu">
-        <a href="/sobre" className="text">Sobre</a>
-        <a href="/quem-somos" className="text">Quem Somos</a>
-        <a href="/entrar" className="text">Entrar</a>
-        <div className="icons">
-        <span><BsYoutube /></span>
-        <span><AiFillLinkedin /></span>
-        <span><AiOutlineInstagram /></span>
+    <div className={`header ${showNav ? 'show-nav' : ''}`}>
+            <div className="logo">
+                <img className='logotipo' src="/logotipo.png" alt="logotipo" />
+            </div>
+            <div className={`menu ${showNav ? 'show' : ''}`}>
+                <div className="menu-toggle" onClick={toggleNav}>
+                    <BsList />
+                </div>
+                <a href="https://www.bemprotege.com" className="text">Sobre</a>
+                <a href="https://www.bemprotege.com" className="text">Quem Somos</a>
+                <a href="https://www.bemprotege.com" className="text">Entrar</a>
+                <div className="icons">
+                    <span><BsYoutube /></span>
+                    <span><AiFillLinkedin /></span>
+                    <span><AiOutlineInstagram /></span>
+                </div>
+            </div>
         </div>
-      </div>
-      </div> 
     
   );
 }
--- src/componentes/main/Main.jsx
+++ src/componentes/main/Main.jsx
@@ -1,44 +1,62 @@
 import "./styleMain.css"
+import React from 'react';
+
+import './styleMain.css';
+
 function Main() {
     return (
-        <div>
-        <div class="main">
-            <div class="content">
-            <h2>ESSENCIAL É ESTAR SEGURO</h2>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu nulla urna.
-                Aenean laoreet sapien vitae libero dapibus, eget condimentum ante accumsan.
-                Morbi tincidunt mauris et egestas molestie.</h6>
-            <button>CONHEÇA AS SOLUÇÕES</button>
+        <div className="main-container">
+            <div className="main">
+                <div className="content">
+                    <h2>ESSENCIAL É ESTAR SEGURO</h2>
+                    <h6>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu nulla urna. Aenean laoreet sapien
+                        vitae libero dapibus, eget condimentum ante accumsan. Morbi tincidunt mauris et egestas molestie.
+                    </h6>
+                    <button>CONHEÇA AS SOLUÇÕES</button>
+                </div>
+                <img src="/gustavolima.png" alt="" />
             </div>
-            <img src="/gustavolima.png" alt="" />
-            </div>
-            <div class="custom-line"></div>
-            <div class="content2">
+            <div className="custom-line"></div>
+            <div className="content2">
                 <img src="/mulherpensando.png" alt="" />
                 <div>
-                    <h1>Sua proteção em primeiro lugar sempre </h1>
-                    <div class="container">
-                <div class="square"><h3>solução 1</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
-        <a className="text-white" href="www.bemprotege.com">Saiba mais</a></div>
-                <div class="square"><h3>solução 2</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
-                            <a className="text-white" href="www.bemprotege.com">Saiba mais</a></div>
-                        <div class="square"><h3>solução 3</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
-                            <a className="text-white" href="www.bemprotege.com">Saiba mais</a></div>
-                        <div class="square"><h3>solução 4</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
-                            <a className="text-white" href="www.bemprotege.com">Saiba mais</a></div>
-                        
-</div>
+                    <h1>Sua proteção em primeiro lugar sempre</h1>
+                    <div className="container">
+                        <div className="square">
+                            <h3>solução 1</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a className="text-white" href="www.bemprotege.com">
+                                Saiba mais
+                            </a>
+                        </div>
+                        <div className="square">
+                            <h3>solução 2</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a className="text-white" href="www.bemprotege.com">
+                                Saiba mais
+                            </a>
+                        </div>
+                        <div className="square">
+                            <h3>solução 3</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a className="text-white" href="www.bemprotege.com">
+                                Saiba mais
+                            </a>
+                        </div>
+                        <div className="square">
+                            <h3>solução 4</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <a className="text-white" href="www.bemprotege.com">
+                                Saiba mais
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        
-            <div class="custom-line"></div>
+            <div className="custom-line"></div>
         </div>
-       
-    
-    )
+    );
 }
+
 export default Main;
